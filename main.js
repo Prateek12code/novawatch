a5acbc809a2b3e7fef5b5cc9ab4171d93073f81a
PrateekShukla <124058592+Prateek12code@users.noreply.github.com>
2023-12-04 17:30:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+itq =  parseInt(document. getElementById("itq"). value)
+val =  parseInt(document. getElementById("value"). value);
+subtotal = itq * val;
+document.getElementById("sub").innerHTML = subtotal; 
